bf6a03b13516ee3c97a346b7f30a6f3cbe4498d2
lukacspapp <papplukacs@hotmail.com>
2022-02-23 11:46:29 +0000

    board design

--- Tic-Tac-Toe/src/app/board/board.component.ts
+++ Tic-Tac-Toe/src/app/board/board.component.ts
@@ -9,7 +9,7 @@ export class BoardComponent implements OnInit {
 
   squares!: any[];
   xIsNext: boolean | undefined
-  winner: string | undefined
+  winner!: string;
 
   constructor() { } // in here 
 
@@ -29,31 +29,37 @@ export class BoardComponent implements OnInit {
   }
 
   makeMove(idx: number) {
-    if (!this.squares[idx]) { // event handler 
-      this.squares?.splice(idx, 1, this.player) // what is the index in the array that was clicked if it has been clicked = null  if not splice in the index 
-      this.xIsNext = !this.xIsNext
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
     }
-    this.winner = this.calulateWinner() {
-      const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [0, 4, 8],
-        [2, 4, 8],
-        [2, 4, 6],
-      ];
-      for (let i = 0; i < lines.length; i++) {
-        const [a , b, c] = lines[i]
-        if (
-          this.squares[a] && this.squares[a] === this.squares[b] && this.squares[a] === this.squares[c]
-        ) {
-          return this.squares[a]
-        }
-      }
-      return null
-    }
+
+    this.winner = this.calculateWinner();
   }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+  
 
 }
